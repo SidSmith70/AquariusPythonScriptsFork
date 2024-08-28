40809c195e79e69dfc7f7241a79479dd8d8df692
Sid Smith <sidsmith70@gmail.com>
2024-08-28 13:48:10 -0400

    factored out a unified watcher

--- OCR/OCRService.py
+++ OCR/OCRService.py
@@ -4,6 +4,7 @@ from config import watcher_type
 from OCRWatcher import OCRWatcher
 from FullTextWatcher import FullTextWatcher
 from OCRFullTextWatcher import OCRFullTextWatcher  
+from UnifiedWatcher import UnifiedWatcher
 
 class OCRService(SMWinservice):
     _svc_name_ = "OCRService"
@@ -12,7 +13,7 @@ class OCRService(SMWinservice):
 
     def start(self):
         self.isrunning = True
-        self.watcher = self.get_watcher(watcher_type) # OCRFullTextWatcher()  # Initialize the watcher here
+        self.watcher = UnifiedWatcher()  # self.get_watcher(watcher_type) # OCRFullTextWatcher()  # Initialize the watcher here
 
     def main(self):
         self.watcher.start()
@@ -21,17 +22,7 @@ class OCRService(SMWinservice):
     def stop(self):
         self.isrunning = False
         self.watcher.stop()  # Properly stop the watcher
-
-    def get_watcher(self, watcher_type):
-        if watcher_type == "FullTextWatcher":
-            
-            return FullTextWatcher()
-        elif watcher_type == "OCRWatcher":
-            
-            return OCRWatcher()
-        else:
-            
-            return OCRFullTextWatcher()
+   
 
 if __name__ == '__main__':
     OCRService.parse_command_line()
